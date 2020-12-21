0e6b5f1b01dc7c3282b502a0b68ed25fbef49108
Kate Weaver <kweave23@jhu.edu>
2020-12-21 12:33:33 -0500

    add window arg to script call

--- slurm_sperm_HMM_kw_20201220.sh
+++ slurm_sperm_HMM_kw_20201220.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=sperm_HMM
 #SBATCH -N 1
-#SBATCH --partition=shared
-#SBATCH ---mem=10GB
+#SBATCH --partition=lrgmem
+#SBATCH ---mem=1008GB
 #SBATCH --time=72:00:00
 #SBATCH --mail-type=all
 #SBATCH --mail-user=kweave23@jhu.edu
@@ -13,4 +13,6 @@
 module load R/4.0.2
 module load gcc/5.5.0
 
-Rscript assign_sperm_haplotypes_kw.R ~/work-rmccoy22/rmccoy22/mccarroll-sperm-seq-data/${SAMPLE_DIR}/data/${SAMPLE_DIR}_goodcellsreplicatebcs_filteredhetsnps_${SLURM_ARRAY_TASK_ID}.cellsbyrow.txt ${SAMPLE_DIR}_chr${SLURM_ARRAY_TASK_ID} 0.005 &> out_assign_sperm_haplotypes_kw_20201220_${SAMPLE_DIR}_${SLURM_ARRAY_TASK_ID}.out
+echo JobID_${SLURM_JOB_ID}_Sample_${SAMPLE_DIR}_chr${SLURM_ARRAY_TASK_ID} > out_assign_sperm_haplotypes_kw_20201220_${SAMPLE_DIR}_${SLURM_ARRAY_TASK_ID}.out
+Rscript assign_sperm_haplotypes_kw.R ~/work-rmccoy22/rmccoy22/mccarroll-sperm-seq-data/${SAMPLE_DIR}/data/${SAMPLE_DIR}_goodcellsreplicatebcs_filteredhetsnps_${SLURM_ARRAY_TASK_ID}.cellsbyrow.txt ${SAMPLE_DIR}_chr${SLURM_ARRAY_TASK_ID} 0.005 2500 &>> out_assign_sperm_haplotypes_kw_20201220_${SAMPLE_DIR}_${SLURM_ARRAY_TASK_ID}.out
+echo DONE >> out_assign_sperm_haplotypes_kw_20201220_${SAMPLE_DIR}_${SLURM_ARRAY_TASK_ID}.out
