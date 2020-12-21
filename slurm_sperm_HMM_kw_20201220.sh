e11b97517d848c020162d5d0f4322c34ae860e91
Kate Weaver <kweave23@jhu.edu>
2020-12-20 23:11:38 -0500

    switch to module load

--- slurm_sperm_HMM_kw_20201220.sh
+++ slurm_sperm_HMM_kw_20201220.sh
@@ -10,8 +10,7 @@
 #SBATCH --array=1-22%22
 #SBATCH --account=rmccoy22
 
-source activate R_env
+module load R/4.0.2
+module load gcc/5.5.0
 
 Rscript assign_sperm_haplotypes_kw.R ~/work-rmccoy22/rmccoy22/mccarroll-sperm-seq-data/${SAMPLE_DIR}/data/${SAMPLE_DIR}_goodcellsreplicatebcs_filteredhetsnps_${SLURM_ARRAY_TASK_ID}.cellsbyrow.txt ${SAMPLE_DIR}_chr${SLURM_ARRAY_TASK_ID} 0.005 &> out_assign_sperm_haplotypes_kw_20201220_${SAMPLE_DIR}_${SLURM_ARRAY_TASK_ID}.out
-
-conda deactivate
