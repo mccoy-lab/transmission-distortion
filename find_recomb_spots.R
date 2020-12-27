2b5dab6ead6540ffa74540a8cf62c60115dd98df
Kate Weaver <kweave23@jhu.edu>
2020-12-27 17:08:46 -0500

    includes an identifier

--- find_recomb_spots.R
+++ find_recomb_spots.R
@@ -12,8 +12,10 @@ overall_test <- cbind(test1, test2, test3, test4, test5, test6)
 #test <- c("h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", NA, NA, "h2", "h1", NA, "h2" ) #this will catch two of the 3 recombination spots
 
 
-find_recomb_spots <- function(input_tib, x) {
+find_recomb_spots <- function(input_tib, x, identities) {
 input_vec <- input_tib[,x]
+ident <- identities[x]
+message(ident)
 single_rs <- FALSE
 
 na_locs <- which(is.na(input_vec)) #which locations have NAs
@@ -21,14 +23,16 @@ num_na_locs <- sum(is.na(input_vec)) #how many NAs
 rle_result <- rle(input_vec)
 
 if ((num_na_locs == 0) & (length(rle_result$values) == 1)) { #no recombination spots
-  return (as_tibble(c()))
+  recomb_spots <- as_tibble(cbind(ident, "None", "None"))
+  colnames(recomb_spots) <- c("Ident", "Start Index", "End Index")
+  return (recomb_spots)
 }
 
 if ((num_na_locs == 0) & (length(rle_result$values) > 1)) { #recombination spots, but no NA's buffering them
   recomb_spot_starts <- lapply(rle_result["lengths"], cumsum)$lengths[1:length(rle_result$values)-1]
   recomb_spot_ends <- recomb_spot_starts + 1
-  recomb_spots <- as_tibble(cbind(recomb_spot_starts, recomb_spot_ends))
-  colnames(recomb_spots) <- c("Start Index", "End Index")
+  recomb_spots <- as_tibble(cbind(ident, recomb_spot_starts, recomb_spot_ends))
+  colnames(recomb_spots) <- c("Ident", "Start Index", "End Index")
   return (recomb_spots)
 }
 
@@ -52,7 +56,7 @@ recomb_spots <- c() #initialize empty list
 if ((last_na_loc - first_na_loc + 1) == num_na_locs) { #there's a single recombination spot
   single_rs <- TRUE
   if (input_vec[first_na_loc-1] != input_vec[last_na_loc + 1]) {
-  recomb_spots <- rbind(recomb_spots, c(first_na_loc - 1, last_na_loc + 1))
+  recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, last_na_loc + 1))
   } else {stop(paste0("Haplotypes surrounding first set of NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", last_na_loc + 1))}
 } else {
   
@@ -60,7 +64,7 @@ if ((last_na_loc - first_na_loc + 1) == num_na_locs) { #there's a single recombi
     first_hp_loc <- which(!is.na(input_vec[first_na_loc_fi : last_na_loc]))[i] #this will find the first haplotype location for each "recombination spot", unless it's the last, then it should return an NA
     if (!is.na(first_hp_loc)) { #for all but the last recombination spots
       if (input_vec[first_na_loc - 1] != input_vec[first_na_loc_fi + first_hp_loc - 1]) {#make sure haplotypes are different
-      recomb_spots <- rbind(recomb_spots, c(first_na_loc - 1, first_na_loc_fi  + first_hp_loc -1)) #add the indices surrounding the recombination spot
+      recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, first_na_loc_fi  + first_hp_loc -1)) #add the indices surrounding the recombination spot
       } else {stop(paste0("Haplotypes surrounding NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", first_na_loc_fi + first_hp_loc - 1))}
   
       i <- i + 1 #add to incremental counter
@@ -76,15 +80,15 @@ if ((last_na_loc - first_na_loc + 1) == num_na_locs) { #there's a single recombi
   
 if (((last_na_loc - first_na_loc + 1) > 0) & !single_rs) { #add last recombination spots
   if (input_vec[first_na_loc - 1] != input_vec[last_na_loc + 1]) { #make sure haplotypes are different
-  recomb_spots <- rbind(recomb_spots, c(first_na_loc - 1, last_na_loc + 1))
+  recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, last_na_loc + 1))
   } else {stop(paste0("Haplotypes surrounding last recombination spot NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", last_na_loc + 1))}
 }
 
 recomb_spots <- as_tibble(recomb_spots)
-colnames(recomb_spots) <- c("Start Index", "End Index")
+colnames(recomb_spots) <- c("Ident", "Start Index", "End Index")
 
 return (recomb_spots)
 
 }
 
-recomb_spots_all <- do.call(rbind, pblapply(1:ncol(overall_test), function(x) find_recomb_spots(overall_test, x)))
+recomb_spots_all <- do.call(rbind, pblapply(1:ncol(overall_test), function(x) find_recomb_spots(overall_test, x, c('test1', 'test2', 'test3', 'test4', 'test5', 'test6'))))
