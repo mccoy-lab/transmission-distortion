f8b0ae771390542701a4667df8beab7c47bef2ac
Kate Weaver <kweave23@jhu.edu>
2020-12-27 19:04:28 -0500

    works on all tests so far except last must think about this one

--- find_recomb_spots.R
+++ find_recomb_spots.R
@@ -9,16 +9,15 @@ test5 <- c("h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", "h2", "h2
 test6 <- c("h1", "h1", "h1", "h1", "h1", "h2", "h2", "h2", "h2", "h2", "h2", "h1", "h1", "h1", "h1")
 overall_test <- cbind(test1, test2, test3, test4, test5, test6)
 #test <- c("h1", "h1", "h1", "h1", "h1", NA, "h1", "h1", NA, NA, "h1", "h1", NA, "h2" ) # this case will cause an error
-#test <- c("h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", NA, NA, "h2", "h1", NA, "h2" ) #this will catch two of the 3 recombination spots
-
+#test <- c("h1", "h1", "h1", "h1", "h1", "h1", "h1", "h1", NA, NA, "h2", "h1", NA, "h2", "h2" ) #this will catch two of the 3 recombination spots
 
 find_recomb_spots <- function(input_tib, x, identities) {
 input_vec <- input_tib[,x]
 ident <- identities[x]
-message(ident)
 single_rs <- FALSE
 
 na_locs <- which(is.na(input_vec)) #which locations have NAs
+message(paste("na_locs: ", na_locs))
 num_na_locs <- sum(is.na(input_vec)) #how many NAs
 rle_result <- rle(input_vec)
 
@@ -37,8 +36,10 @@ if ((num_na_locs == 0) & (length(rle_result$values) > 1)) { #recombination spots
 }
 
 neighboring_difs <- diff(na_locs)
+message(paste("neighboring_difs: ", neighboring_difs))
 
 subsetted_na_locs <- c(na_locs[1])
+message(paste("subsetted_na_locs: ", subsetted_na_locs))
 for (i in 2:length(na_locs)){
   if (neighboring_difs[i-1] != 1){
     subsetted_na_locs <- cbind(subsetted_na_locs, na_locs[i]) #need this for when we have different number of NAs, especially more than 1 between the different haplotypes
@@ -57,19 +58,32 @@ if ((last_na_loc - first_na_loc + 1) == num_na_locs) { #there's a single recombi
   single_rs <- TRUE
   if (input_vec[first_na_loc-1] != input_vec[last_na_loc + 1]) {
   recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, last_na_loc + 1))
+  message(paste('Added spot ', ident, first_na_loc -1, last_na_loc + 1))
   } else {stop(paste0("Haplotypes surrounding first set of NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", last_na_loc + 1))}
 } else {
   
   while ((last_na_loc - first_na_loc + 1) > num_na_locs) { #means that we must have some haplotypes in the middle or there must be more than one recombination spot
     first_hp_loc <- which(!is.na(input_vec[first_na_loc_fi : last_na_loc]))[i] #this will find the first haplotype location for each "recombination spot", unless it's the last, then it should return an NA
+    message(paste("first_hp_loc: " , first_hp_loc))
+    message(paste("i: ", i))
+    message(paste("first_na_loc -1: ", first_na_loc-1))
+    message(paste("first_na_loc_fi + first_hp_loc -1: ", first_na_loc_fi+first_hp_loc -1))
+    message(paste("first_na_loc_fi: ", first_na_loc_fi))
+    message(paste("num_na_locs: ", num_na_locs))
     if (!is.na(first_hp_loc)) { #for all but the last recombination spots
       if (input_vec[first_na_loc - 1] != input_vec[first_na_loc_fi + first_hp_loc - 1]) {#make sure haplotypes are different
       recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, first_na_loc_fi  + first_hp_loc -1)) #add the indices surrounding the recombination spot
+      message(paste('Added spot ', ident, first_na_loc -1, first_na_loc_fi+first_hp_loc-1))
       } else {stop(paste0("Haplotypes surrounding NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", first_na_loc_fi + first_hp_loc - 1))}
   
       i <- i + 1 #add to incremental counter
       first_na_loc <- subsetted_na_locs[i] #find new first na for next recombination spot
-      num_na_locs <- sum(is.na(input_vec[(first_na_loc + first_hp_loc-1):length(input_vec)])) #find new number of remaining/unaccounted for NAs
+      message(paste("new first_na_loc: ", first_na_loc))
+      num_na_locs <- sum(is.na(input_vec[(first_na_loc_fi + first_hp_loc-1):length(input_vec)])) #find new number of remaining/unaccounted for NAs
+      message(paste("new num_na_locs: ", num_na_locs))
+      if (num_na_locs == 0){ #this also means reached last recombination spot
+        break
+      }
             
       }
     else { #reached last recombination spots
@@ -81,6 +95,7 @@ if ((last_na_loc - first_na_loc + 1) == num_na_locs) { #there's a single recombi
 if (((last_na_loc - first_na_loc + 1) > 0) & !single_rs) { #add last recombination spots
   if (input_vec[first_na_loc - 1] != input_vec[last_na_loc + 1]) { #make sure haplotypes are different
   recomb_spots <- rbind(recomb_spots, c(ident, first_na_loc - 1, last_na_loc + 1))
+  message(paste('Added spot ', ident, first_na_loc -1, last_na_loc + 1))
   } else {stop(paste0("Haplotypes surrounding last recombination spot NAs match but appear to be a recombination spot at ", first_na_loc -1, " and ", last_na_loc + 1))}
 }
 
