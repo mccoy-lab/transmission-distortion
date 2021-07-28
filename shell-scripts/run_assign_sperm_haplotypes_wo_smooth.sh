2a023829d33d7e99e3cfc97f66cd739302870388
Kate Weaver <kweave23@jhu.edu>
2021-07-28 17:59:23 -0400

    move shell script

---
+++ shell-scripts/run_assign_sperm_haplotypes_wo_smooth.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for NUM in {1..22}
+do
+  Rscript assign_sperm_haplotypes_wo_smooth.R /home/kweave23/gamete_data/filterhet/$1/${1}_euploid_${NUM}.txt /home/kweave23/gamete_data/misc_inputs/encode_blacklist.bed.gz /home/kweave23/gamete_data/misc_inputs/giab_union_regions.bed $1 $NUM /home/kweave23/gamete_data/run_bell_20210302/${1}/csv_out/ 5 &> /home/kweave23/gamete_data/run_bell_20210302/${1}/out_${1}_${NUM}_with_filter_20210302.txt
+done
